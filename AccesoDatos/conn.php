5fdc81b725e255a3a5115447e31936f6cff1fd42
Gastón Marcilio <gaston.marcilio@gmail.com>
2020-12-22 23:38:23 -0300

    migracion
    migracion nuevo server. cambio de conexion y script de la bbdd

--- AccesoDatos/conn.php
+++ AccesoDatos/conn.php
@@ -4,7 +4,7 @@
 dev = 1
 produccion = 2
 */
-$entorno = 1;
+$entorno = 2;
 
 if($entorno == 1){
 
@@ -26,15 +26,16 @@ if($entorno == 1){
 
   try {
 
-    $dbname = "monichei_wp837";
-    $dsn = "mysql:host=localhost;dbname=$dbname";
+    $dbname = "perlattohelados_com_ar";
+    $dsn = "mysql:host=aplin.amalthea.dreamhost.com;dbname=$dbname";
     $options = array(
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     );
-    $user = "monichei_wp837";
-    $password = "5S-4uCp95@";
+    $user = "perlattoheladosc";
+    $password = "W3Rfxz!y";
     $dbh = new PDO($dsn, $user, $password);
   } catch (PDOException $e){
+    echo 'hay un error en la conexion';
     echo $e->getMessage();
   }
   
